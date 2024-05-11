8cd7063227b6869c21d26c390cf656a1b95bd2c2
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2024-05-11 17:28:29 -0300

    Navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import './font/stylesheet.css'
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
-      <h1>App</h1>
+      <Navbar />
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,29 @@ import { HiRectangleGroup } from "react-icons/hi2";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <header className="">
+            <nav className="navbar navbar-expand-lg navbar-dark d-flex py-4">
+                <div className="container-fluid p-0">
+                    <a className="navbar-brand flex-grow-1 fw-semibold fs-5" href="#">compleet labs</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                        <div className="navbar-nav fs-5 gap-3 ">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" href="#services">Services</a>
+                            <a className="nav-link" href="#tech">Technology</a>
+                            <a className="nav-link" href="#brands">Case studies</a>
+                            <a className="nav-link" href="#tech">About</a>
+                            <div className="d-flex align-items-center">
+                                <HiRectangleGroup size={20} />
+                                <a className="nav-link active" href="#"> Hire us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header >
     )
 }
 
